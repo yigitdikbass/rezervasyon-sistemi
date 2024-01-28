f56c73f325ce3b34af6b29000135ca7b5ab249a0
yigitdikbass <153829751+yigitdikbass@users.noreply.github.com>
2024-01-28 13:49:07 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/prolab/ProLab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prolab;
+
+/**
+ *
+ * @author Yiğit
+ */
+public class ProLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
